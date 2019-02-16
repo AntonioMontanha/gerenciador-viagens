62db12819bfb26f942cc6fa24b2be0a135e60ae3
Julio de Lima <iam@juliodelima.com.br>
2019-02-16 16:38:39 -0200

    add: filter to search trips by location

--- src/main/java/com/montanha/gerenciador/repositories/ViagemRepository.java
+++ src/main/java/com/montanha/gerenciador/repositories/ViagemRepository.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.montanha.gerenciador.entities.Viagem;
 
+import java.util.List;
+
 @Repository
 public interface ViagemRepository extends JpaRepository<Viagem, Long> {
 
 	Viagem findByLocalDeDestino(String localDeDestino);
+
+	List<Viagem> findAllByLocalDeDestino(String localDeDestino);
 	
 
 }
